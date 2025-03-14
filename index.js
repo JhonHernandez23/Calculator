c06801d9c9d8f0fc4db61bf7cc6ec01d6fb344fb
JhonHernandez23 <jhoneduardhernandezespana50@gmail.com>
2025-03-14 11:06:28 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,38 +1,45 @@
 const pantalla = document.querySelector(".pantalla");
 const buttons = document.querySelectorAll(".btn");
-buttons.forEach(button => {
+let limpiarPantalla = false;
+buttons.forEach((button) => {
     button.addEventListener("click", () => {
-       const buttonSelect = button.textContent;
-       //Función borrar todos los valores
-       if(button.id === "borrarTodo"){
-        pantalla.textContent = "0";
-        return;
-       }
-       //Función borrar de un digito
-       if(button.id === "borrar"){
-                if(pantalla.textContent.length === 1 || pantalla.textContent === "error"){
-                pantalla.textContent = "0"
-            }
-                else{
-                pantalla.textContent = pantalla.textContent.slice(0, -1)
-            }
-                return;
-       }
-       //Función de igualdad
-       if(button.id === "igual"){
-        try{
-        pantalla.textContent = eval(pantalla.textContent);
-        } catch{
-            pantalla.textContent = "error";
+        const buttonSelect = button.textContent;
+        //Función borrar todos los valores
+        if (button.id === "borrarTodo") {
+            pantalla.textContent = "0";
+            return;
         }
-        return;
-       }
-
-       if(pantalla.textContent === "0" || pantalla.textContent === "error"){
-        pantalla.textContent = buttonSelect;
-       }
-       else {
-        pantalla.textContent += buttonSelect;
-       }
-    })
-})
+        //Función borrar de un digito
+        if (button.id === "borrar") {
+            if (
+                pantalla.textContent.length === 1 ||
+                pantalla.textContent === "error"
+            ) {
+                pantalla.textContent = "0";
+            } else {
+                pantalla.textContent = pantalla.textContent.slice(0, -1);
+            }
+            return;
+        }
+        //Función de igualdad
+        if (button.id === "igual") {
+            try {
+                pantalla.textContent = eval(pantalla.textContent);
+                limpiarPantalla = true;
+            } catch {
+                pantalla.textContent = "error";
+            }
+            return;
+        }
+        if (limpiarPantalla) {
+            pantalla.textContent = buttonSelect;
+            limpiarPantalla = false;
+            return;
+        }
+        if (pantalla.textContent === "0" || pantalla.textContent === "error") {
+            pantalla.textContent = buttonSelect;
+        } else {
+            pantalla.textContent += buttonSelect;
+        }
+    });
+});
